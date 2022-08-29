606fec68ec35369634305cbe391853ce95a338f8
TRP <tirth4ur@gmail.com>
2022-08-29 15:53:46 -0400

    Cleanup

--- App/App.cpp
+++ App/App.cpp
@@ -1,57 +1,19 @@
-/*
- * Copyright (C) 2011-2021 Intel Corporation. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *   * Neither the name of Intel Corporation nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
+
 
 // App.cpp : Define the entry point for the console application.
-//
-
-// I ADD
 
 #include "App.h"
 #include <string.h>
-
-//I ADDED TOP
-
 #include <string.h>
 #include <assert.h>
 #include <fstream>
 #include <thread>
 #include <iostream>
-
 #include "sgx_urts.h"
 #include "Enclave_Seal_u.h"
 #include "Enclave_Unseal_u.h"
-
 #include "ErrorSupport.h"
 using namespace std;
-//#include "../AppInitiator/App.cpp"
 #define ENCLAVE_NAME_SEAL "libenclave_seal.signed.so"
 #define ENCLAVE_NAME_UNSEAL "libenclave_unseal.signed.so"
 #define SEALED_DATA_FILE "sealed_data_blob.txt"
@@ -110,7 +72,6 @@ using namespace std;
     return true;
 }
 
-// Initialize the enclave:
 //   Call sgx_create_enclave to initialize an enclave instance
 
  sgx_status_t initialize_enclave(const char* enclave_path, sgx_enclave_id_t *eid)
@@ -261,6 +222,9 @@ void exportSealInfo(char *fileName, uint8_t *buf, uint32_t data_size){
     }
 }
 
+
+
+
 //gets initial info and sends to enclave to be encrypted
  void getInitialInfo(){
 
@@ -282,18 +246,16 @@ void exportSealInfo(char *fileName, uint8_t *buf, uint32_t data_size){
     sgx_destroy_enclave(eid_seal);
 
 }
-
+//helper function
 void printPin(int *pin){
     printf("PIN: %d\n", *pin);
 }
-
+//helper function
 char *intToString(int *num){
-    //printf("NUM: %d\n", *num);
     int convert = *num;
     char ret[15];
     sprintf(ret, "%d", convert);
     char *x = ret;
-    //printf("num: %s\n", x);
     return x;
 }
 
@@ -402,18 +364,10 @@ void printMem(uint8_t *str, uint32_t data_size){
     (void)data_size;
 }
 
+//helper function
 void ocall_print_string(const char *str)
 {
-    /* Proxy/Bridge will check the length and null-terminate
-     * the input string to prevent buffer overflow.
-     */
     printf("%s\n: ", str);
-
- /*   for(long unsigned int i = 0; i < sizeof(str); i++){
-        if((int)str[i] < 0) break;
-        printf("%d\n", str[i]);
-
-    }*/
 }
 
 int getDeposit(){
