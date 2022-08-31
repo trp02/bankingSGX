cd800009428964bc8a4b9149100235f70239fec9
TRP <83850231+trp02@users.noreply.github.com>
2022-08-31 00:37:36 -0400

    Add files via upload

--- Enclave_Unseal/Enclave_Unseal.cpp
+++ Enclave_Unseal/Enclave_Unseal.cpp
@@ -1,33 +1,7 @@
 /*
- * Copyright (C) 2011-2021 Intel Corporation. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *   * Neither the name of Intel Corporation nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
+Unsealing enclave: Unseals data, processes transactions and reseals
+
+*/
 #include "Enclave_Unseal_t.h"
 
 #include "sgx_trts.h"
@@ -79,6 +53,8 @@ sgx_status_t unseal_data(const uint8_t *sealed_blob, size_t data_size)
         free(de_mac_text);
         return SGX_ERROR_OUT_OF_MEMORY;
     }
+
+    //unseal data and store it in sealed blob
     sgx_status_t ret = sgx_unseal_data((const sgx_sealed_data_t *)sealed_blob, de_mac_text, &mac_text_len, decrypt_data, &decrypt_data_len);
     if (ret != SGX_SUCCESS)
     {
@@ -86,8 +62,6 @@ sgx_status_t unseal_data(const uint8_t *sealed_blob, size_t data_size)
         free(decrypt_data);
         return ret;
     }
-
-
 
 
     const char *convertCurText = (char*)decrypt_data;
@@ -170,16 +144,6 @@ sgx_status_t unseal_data(const uint8_t *sealed_blob, size_t data_size)
     bankInfo = {fname, lname, blnceConverted, pin};
 
 
-
-   // curText = (uint8_t*)u;
-   // printMem(curText, 100);
-
-/*
-    if (memcmp(de_mac_text, aad_mac_text, strlen(aad_mac_text)) || memcmp(decrypt_data, encrypt_data, strlen(encrypt_data)))
-    {
-        ret = SGX_ERROR_UNEXPECTED;
-    }
-*/
     free(de_mac_text);
     free(decrypt_data);
     return ret;
@@ -220,15 +184,22 @@ char* concatData(){
     double moveDec = bankInfo.balance * 100;
     int blnce = (int) moveDec;
     char *b;  
-    intToString(&b, &blnce);
+
+    //POINTER DEMO
+    int testExam = 33;
+    intToString(&b, &blnce, &testExam);
+    //POINTER DEMO
     int bCheck = atoi(b);
-
     dataToEncrypt = concat(dataToEncrypt, b);
-
     //pin 
     int pin = bankInfo.pin;
     char *p;
-    intToString(&p, &pin);
+
+
+    //POINTER DEMO
+    int testExam2 = 4;
+    intToString(&p, &pin, &testExam2);
+    //POINTER DEMO
     int pCheck = atoi(p);
     dataToEncrypt = concat(dataToEncrypt, space);
     dataToEncrypt = concat(dataToEncrypt, p);
@@ -242,14 +213,9 @@ char* concatData(){
     return dataToEncrypt;
 }
 
-//get data size to be sealed
-uint32_t get_sealed_data_size()
-{
-    return sgx_calc_sealed_data_size((uint32_t)strlen(aad_mac_text), (uint32_t)strlen(encrypt_data));
-}
-
 
 void sealUpdate(){
+    
 
     char* connectedData = concatData();
     //memcpy(aad_mac_text, connectedData, strlen(connectedData));
@@ -259,17 +225,19 @@ void sealUpdate(){
 
     //will hold sealed data
     uint8_t *temp_sealed_buf = (uint8_t *)malloc(sealed_data_size);
-    sgx_status_t  err = sgx_seal_data((uint32_t)strlen(aad_mac_text), (const uint8_t *)aad_mac_text, (uint32_t)strlen(encrypt_data), (uint8_t *)encrypt_data, sealed_data_size, (sgx_sealed_data_t *)temp_sealed_buf);
-   
 
-    if (err == SGX_SUCCESS)
+    sgx_status_t  ret = sgx_seal_data((uint32_t)strlen(aad_mac_text), (const uint8_t *)aad_mac_text, (uint32_t)strlen(encrypt_data), (uint8_t *)encrypt_data, sealed_data_size, (sgx_sealed_data_t *)temp_sealed_buf);
+
+
+    if (ret == SGX_SUCCESS)
     {
         char file[] = ".txt";
         char *fConcat = file;
         char *seal_file = concat(bankInfo.firstname, fConcat);
         exportSealInfo(seal_file, temp_sealed_buf, sealed_data_size);
-        // Copy the sealed data to outside buffer
-        //memcpy(sealed_blob, temp_sealed_buf, sealed_data_size);
+    }
+    else{
+        abortPro(ret);
     }
     free(temp_sealed_buf);
 }
@@ -279,7 +247,7 @@ int processTransaction(int tChoice){
         if(tChoice == 1){// For deposits
             int dep;
             getDeposit(&dep);
-            bankInfo.balance += dep;
+            bankInfo.balance = bankInfo.balance + (double)((double)dep/100);
             sealUpdate();
             return 1;
 
@@ -287,9 +255,10 @@ int processTransaction(int tChoice){
         else if(tChoice == 2){// For withdrawls
             int with;
             getWithdraw(&with);
-            int allowed = (int)bankInfo.balance - with;
+            double withdraw = (double)with / 100;
+            double allowed = bankInfo.balance - withdraw;
             if(allowed > 0){
-                bankInfo.balance -= with;
+                bankInfo.balance -= withdraw;
                 sealUpdate();
                 return 1;
             }
